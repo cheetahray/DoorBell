226a72ca573b0f9d145f05234469691b41394042
ray <ray@dac.tw>
2014-10-29 19:10:55 +0800

    加注釋

--- pysrc/doorbell.py
+++ pysrc/doorbell.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#|R|a|s|p|b|e|r|r|y|P|i|.|c|o|m|.|t|w|
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#+-+-+-+-+-+-+
+#|d|a|c|.|t|w|
+#+-+-+-+-+-+-+
 #
-# pir.py
-# Sense human and print message with PIR(Passive InfraRed Sensor) sensor
+# doorbell.py
+# Read a button input to trigger midi play then do some GPIO outputs.
 #
-# Author : sosorry
-# Date   : 06/22/2014
+# Author : Digital Art Center, Taipei.
+# Date   : 11/18/2014
 
 import RPi.GPIO as GPIO
 import time
@@ -15,19 +15,19 @@ import unittest
 from mido import MidiFile
 
 mid = MidiFile('song.mid')
-debug = True
-isplay = False
-BUT_PIN = 7
-BOUNCE_TIME = 200
-MAG1_PIN = 11   #PL2 green
-MAG2_PIN = 12   #PL3 yellow
-MAG3_PIN = 13   #PL1 orange
-MAG4_PIN = 15   #PL5 white
-MAG5_PIN = 16   #PL7 purpal
-MAG6_PIN = 18   #PL6 gray
-MAG7_PIN = 22   #PL4 blue
+debug = True        #Boolean for on/off our debug print 
+isplay = False      #Boolean to judge whether the midi is playing
+BUT_PIN = 7         #Doorbell button GPIO input pin number
+BOUNCE_TIME = 200   #GPIO input delay time
+MAG1_PIN = 11       #GPIO pin number output to PL1 red
+MAG2_PIN = 12       #GPIO pin number output to PL2 orange
+MAG3_PIN = 13       #GPIO pin number output to PL3 yellow
+MAG4_PIN = 15       #GPIO pin number output to PL4 green
+MAG5_PIN = 16       #GPIO pin number output to PL5 blue
+MAG6_PIN = 18       #GPIO pin number output to PL6 
+MAG7_PIN = 22       #GPIO pin number output to PL7 purple
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)    #Set GPIO input/output
 GPIO.setup(BUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(MAG1_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(MAG2_PIN, GPIO.OUT, initial=GPIO.LOW)
@@ -37,7 +37,7 @@ GPIO.setup(MAG5_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(MAG6_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(MAG7_PIN, GPIO.OUT, initial=GPIO.LOW)
 
-pwm_mag1 = GPIO.PWM(MAG1_PIN, 70)
+pwm_mag1 = GPIO.PWM(MAG1_PIN, 70)   #Initial PWM
 pwm_mag1.start(0)
 pwm_mag2 = GPIO.PWM(MAG2_PIN, 70)
 pwm_mag2.start(0)
@@ -53,10 +53,10 @@ pwm_mag7 = GPIO.PWM(MAG7_PIN, 70)
 pwm_mag7.start(0)
 
 class Tests(unittest.TestCase):
-    def test_0(self):
+    def test_0(self):   #Test play midi file
         play_midi();
         
-    def test_1(self):
+    def test_1(self):   #Test 1st magnet for 10 times
         for i in range(1,10):
             if debug:
                 print(i)
@@ -65,7 +65,7 @@ class Tests(unittest.TestCase):
             pwm_mag1.ChangeDutyCycle(100)
             time.sleep(0.5)
         
-    def test_2(self):
+    def test_2(self):   #Test 2nd magnet for 10 times
         for i in range(1,10):
             if debug:
                 print(i)
@@ -74,7 +74,7 @@ class Tests(unittest.TestCase):
             pwm_mag2.ChangeDutyCycle(100)
             time.sleep(0.5)
         
-    def test_3(self):
+    def test_3(self):   #Test 3rd magnet for 10 times
         for i in range(1,10):
             if debug:
                 print(i)
@@ -83,7 +83,7 @@ class Tests(unittest.TestCase):
             pwm_mag3.ChangeDutyCycle(100)
             time.sleep(0.5)
         
-    def test_4(self):
+    def test_4(self):   #Test 4th magnet for 10 times
         for i in range(1,10):
             if debug:
                 print(i)
@@ -92,7 +92,7 @@ class Tests(unittest.TestCase):
             pwm_mag4.ChangeDutyCycle(100)
             time.sleep(0.5)
         
-    def test_5(self):
+    def test_5(self):   #Test 5th magnet for 10 times
         for i in range(1,10):
             if debug:
                 print(i)
@@ -101,7 +101,7 @@ class Tests(unittest.TestCase):
             pwm_mag5.ChangeDutyCycle(100)
             time.sleep(0.5)
         
-    def test_6(self):
+    def test_6(self):   #Test 6th magnet for 10 times
         for i in range(1,10):
             if debug:
                 print(i)
@@ -110,7 +110,7 @@ class Tests(unittest.TestCase):
             pwm_mag6.ChangeDutyCycle(100)
             time.sleep(0.5)
     
-    def test_7(self):
+    def test_7(self):   #Test 7th magnet for 10 times
         for i in range(1,10):
             if debug:
                 print(i)
@@ -121,78 +121,78 @@ class Tests(unittest.TestCase):
     
 def play_midi():
     global isplay
-    for message in mid.play():
+    for message in mid.play():  #Next note from midi in this moment
         isplay = False
         if debug:
             print(message)
         if 'note_on' == message.type :
             if 60 == message.note :
                 if 0 == message.velocity :
-                    pwm_mag1.ChangeDutyCycle(0)
+                    pwm_mag1.ChangeDutyCycle(0)     #Do off
                 else :
-                    pwm_mag1.ChangeDutyCycle(100)
+                    pwm_mag1.ChangeDutyCycle(100)   #Do on
             elif 61 == message.note :
                 if 0 == message.velocity :
-                    pwm_mag2.ChangeDutyCycle(0)
+                    pwm_mag2.ChangeDutyCycle(0)     #Re off
                 else :
-                    pwm_mag2.ChangeDutyCycle(100)
+                    pwm_mag2.ChangeDutyCycle(100)   #Re on
             elif 62 == message.note :
                 if 0 == message.velocity :
-                    pwm_mag3.ChangeDutyCycle(0)
+                    pwm_mag3.ChangeDutyCycle(0)     #Mi off
                 else :
-                    pwm_mag3.ChangeDutyCycle(100)    
+                    pwm_mag3.ChangeDutyCycle(100)   #Mi on
             elif 63 == message.note :
                 if 0 == message.velocity :
-                    pwm_mag4.ChangeDutyCycle(0)
+                    pwm_mag4.ChangeDutyCycle(0)     #Fa off
                 else :
-                    pwm_mag4.ChangeDutyCycle(100)
+                    pwm_mag4.ChangeDutyCycle(100)   #Fa on
             elif 64 == message.note :
                 if 0 == message.velocity :
-                    pwm_mag5.ChangeDutyCycle(0)
+                    pwm_mag5.ChangeDutyCycle(0)     #So off
                 else :
-                    pwm_mag5.ChangeDutyCycle(100)
+                    pwm_mag5.ChangeDutyCycle(100)   #So on
             elif 65 == message.note :
                 if 0 == message.velocity :
-                    pwm_mag6.ChangeDutyCycle(0)
+                    pwm_mag6.ChangeDutyCycle(0)     #La off
                 else :
-                    pwm_mag6.ChangeDutyCycle(100)
+                    pwm_mag6.ChangeDutyCycle(100)   #La on
             elif 66 == message.note :
                 if 0 == message.velocity :
-                    pwm_mag7.ChangeDutyCycle(0)
+                    pwm_mag7.ChangeDutyCycle(0)     #Ti off
                 else :
-                    pwm_mag7.ChangeDutyCycle(100)
+                    pwm_mag7.ChangeDutyCycle(100)   #Ti on
         elif 'note_off' == message.type :
             if 60 == message.note :
-                pwm_mag1.ChangeDutyCycle(0)
+                pwm_mag1.ChangeDutyCycle(0)         #Do off
             elif 61 == message.note :
-                pwm_mag2.ChangeDutyCycle(0)
+                pwm_mag2.ChangeDutyCycle(0)         #Re off
             elif 62 == message.note :
-                pwm_mag3.ChangeDutyCycle(0)    
+                pwm_mag3.ChangeDutyCycle(0)         #Mi off
             elif 63 == message.note :
-                pwm_mag4.ChangeDutyCycle(0)
+                pwm_mag4.ChangeDutyCycle(0)         #Fa off
             elif 64 == message.note :
-                pwm_mag5.ChangeDutyCycle(0)
+                pwm_mag5.ChangeDutyCycle(0)         #So off
             elif 65 == message.note :
-                pwm_mag6.ChangeDutyCycle(0)
+                pwm_mag6.ChangeDutyCycle(0)         #La off
             elif 66 == message.note :
-                pwm_mag7.ChangeDutyCycle(0)
+                pwm_mag7.ChangeDutyCycle(0)         #Ti off
                 
 def callback_function(channel):
     global isplay
-    isplay = True
+    isplay = True   #Switch on the boolean of midi play 
     
 try:
     GPIO.add_event_detect(BUT_PIN, GPIO.RISING, callback=callback_function, bouncetime=BOUNCE_TIME)
-
+                    #Register the door bell button GPIO input call back function
     if '__main__' == __name__ :
-        midi_suite = unittest.TestSuite()
-        _1st_suite = unittest.TestSuite()
-        _2st_suite = unittest.TestSuite()
-        _3st_suite = unittest.TestSuite()
-        _4st_suite = unittest.TestSuite()
-        _5st_suite = unittest.TestSuite()
-        _6st_suite = unittest.TestSuite()
-        _7st_suite = unittest.TestSuite()
+        midi_suite = unittest.TestSuite()   #Add play midi test function
+        _1st_suite = unittest.TestSuite()   #Add 1st magnet test function
+        _2st_suite = unittest.TestSuite()   #Add 2nd magnet test function
+        _3st_suite = unittest.TestSuite()   #Add 3rd magnet test function
+        _4st_suite = unittest.TestSuite()   #Add 4th magnet test function
+        _5st_suite = unittest.TestSuite()   #Add 5th magnet test function
+        _6st_suite = unittest.TestSuite()   #Add 6th magnet test function
+        _7st_suite = unittest.TestSuite()   #Add 7th magnet test function
         all_suite = unittest.TestSuite()
         midi_suite.addTest(Tests("test_0"))
         _1st_suite.addTest(Tests("test_1"))
@@ -216,6 +216,8 @@ try:
             if debug:
                 print isplay 
             play_midi()
+        else
+            time.delay(1)
 
 except KeyboardInterrupt:
     print "Cleaning up the GPIO" 
