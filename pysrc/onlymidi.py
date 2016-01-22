87459b0171643de8999adc3d7215642dc6e31d08
ray@dac.tw <ray@dac.tw>
2016-01-23 00:23:23 +0800

    S A T B 四個表

--- pysrc/onlymidi.py
+++ pysrc/onlymidi.py
@@ -14,7 +14,7 @@ import unittest
 from mido import MidiFile
 import xlsxwriter
 
-mid = MidiFile('matti-lights-6.mid')
+mid = MidiFile('C:\\Windows.old\\Users\\Ray\\Music\\Shanghai\\newyear.mid')
 debug = True        #Boolean for on/off our debug print 
 isplay = False      #Boolean to judge whether the midi is playing
 
@@ -28,18 +28,34 @@ def play_midi():
     global myshift
     workbook = xlsxwriter.Workbook('demo.xlsx')
     worksheet = workbook.add_worksheet()
-    f = []
+    f = [[],[],[],[]]
     for i in range(127):
-        f.append(0)
+        f[0].append(0)
         worksheet.write(i, 0, 0)
+        f[1].append(0)
+        worksheet.write(i, 1, 0)
+        f[2].append(0)
+        worksheet.write(i, 2, 0)
+        f[3].append(0)
+        worksheet.write(i, 3, 0)
     for message in mid.play():  #Next note from midi in this moment
         isplay = False          #To avoid duplicate doorbell button press during midi play
         if debug:
             print(message)
         if 'note_on' == message.type :
             if 0 != message.velocity :
-                f[message.note] = f[message.note] + 1
-                worksheet.write(message.note, 0, f[message.note])
+                if 0 == message.channel:
+                    f[0][message.note] = f[0][message.note] + 1
+                    worksheet.write(message.note, 0, f[0][message.note])
+                elif 1 == message.channel:
+                    f[1][message.note] = f[1][message.note] + 1
+                    worksheet.write(message.note, 1, f[1][message.note])
+                elif 2 == message.channel:
+                    f[2][message.note] = f[2][message.note] + 1
+                    worksheet.write(message.note, 2, f[2][message.note])
+                elif 3 == message.channel:
+                    f[3][message.note] = f[3][message.note] + 1
+                    worksheet.write(message.note, 3, f[3][message.note])
     workbook.close()
     
 try:
